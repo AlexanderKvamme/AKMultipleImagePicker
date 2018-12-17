1a9c8fd39d2ec107dae613556823e6c378ff1a70
Alexander Kvamme <alexanderkvamme@gmail.com>
2018-12-17 13:36:28 +0100

    initial commit

---
+++ ---/AKImageCollectionView.swift
@@ -0,0 +1,14 @@
+//
+//  AKImageCollectionView.swift
+//  AKMultipleImagePicker
+//
+//  Created by Amia Macone on 17/12/2018.
+//  Copyright © 2018 Amia Macone. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+final class AKImageCollectionView: UICollectionView {
+    
+}
---
+++ ---/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  AKMultipleImagePicker
+//
+//  Created by Amia Macone on 17/12/2018.
+//  Copyright © 2018 Amia Macone. All rights reserved.
+//
+
+import UIKit
+
+class AKMultipleImagePickerController: UIViewController {
+
+    // MARK: - Properties
+    
+    private var imagePickerCollectionView = AKImageCollectionView()
+    
+    // MARK: - Initializers
+
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+    }
+
+    // MARK: - Methods
+    
+    // MARK: - Protocol Conformance
+    
+}
+
